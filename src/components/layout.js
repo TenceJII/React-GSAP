9b146592b31cb1b7ef45ed6b6d577b4b56f15b4d
TenceJII <109458478+TenceJII@users.noreply.github.com>
2024-02-15 21:58:21 +0800

    Component
    Here I provide some updates about:
    1. add header and footer using components
    2. create and add a loader base

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import Footer from "./footer";
 import Header from "./header";
 
 const Layout = ({ title, renderHeaderAndFooter, children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Let create async method to fetch fake data
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+
+    fakeDataFetch();
+  }, []);
   return (
     <>
       <Helmet>
-        <title> {title} </title>
+        <title>{title}</title>
       </Helmet>
-      <Header />
-      <main> {children} </main>
-      {renderHeaderAndFooter && <Footer />}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {renderHeaderAndFooter && <Header />}
+          <main>{children}</main>
+          {renderHeaderAndFooter && <Footer />}
+        </>
+      )}
     </>
   );
 };
